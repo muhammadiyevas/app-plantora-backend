6c4845221d886e7d871bca50b7a24b0f866ea7c6
MuhammadiyevaSevinch <muhammadiyeva133@gmail.com>
2024-04-05 15:22:06 +0500

    init commit

---
+++ src/main/java/com/example/appplantorabackend/AppPlantoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.appplantorabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPlantoraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppPlantoraBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appplantorabackend/AppPlantoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appplantorabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPlantoraBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
